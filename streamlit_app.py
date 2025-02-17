c60dac33a96329eca47a9bfd7303a53dda8de0a0
TcrewJamik <117645643+TcrewJamik@users.noreply.github.com>
2025-02-17 18:52:58 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title('🎈 App Name')
 
 st.write('Hello world!')
-df = pd.read_csv('https://raw.gitbubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 
 with st.expander('Data'):
   st.write('X')
