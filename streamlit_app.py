7cbff8b866caae4c902a33eb2163b75e65941c06
TcrewJamik <117645643+TcrewJamik@users.noreply.github.com>
2025-02-17 19:37:07 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,27 @@ fig2 = px.histogram(
   title = 'Distribution')
 
 st.plotly_chart(fig2)
+
+
+with st.expander('Input features'):
+  st.write('**Imput penguin**')
+  st.dataframe(input_df)
+  st.write('**Combined penguins data** (Input row + original data)
+  st.dataframe(input_penguins)
+
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix = encode)
+
+X = df_penguins[1:]
+input_row = df_penguins[1:]
+
+target_mapper = {'Adelit': 0, 'Chinstrap': 1, 'Gentoo': 2)
+def target_encode(val):
+  return target_mapper[val]
+y = y_raw.apply(target_encode)
+
+with st.expander('Data preparation'):
+  st.write('**Encoded X (input penguin)**')
+  st.dataframe(input_row)
+  st.write('**Encoded y**')
+  st.write(y)
